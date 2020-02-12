df77e197e2270e49d9c3d1bf05a4c1ae36e07871
Wojtek Pogorzelski <w.pogorzelski95@gmail.com>
2020-02-12 15:21:56 +0000

    First App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'root-directive',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my-first-app';
+
+
+  name:string ="Wojtek";
+  age:number = 25;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ComponentaComponent } from './componenta/componenta.component';
+import { ComponentbComponent } from './componentb/componentb.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentaComponent,
+    ComponentbComponent
   ],
   imports: [
     BrowserModule
